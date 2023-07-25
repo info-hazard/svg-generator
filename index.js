976678b9bc92caf7c4434d3ceaf7b8083c30032e
info-hazard <107716504+info-hazard@users.noreply.github.com>
2023-07-24 18:12:52 -0700

    added inquirer prompts to render SVGs based on user input

--- index.js
+++ index.js
@@ -1,30 +1,41 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
+const {Circle, Square, Triangle} = require("./lib/shapes");
 
-inquirer
-  .prompt([
-    {
-      type: 'input',
-      message: 'Please enter up to 3 characters.',
-      name: 'text',
-    },
-    {
-        type: 'input',
-        message: 'Please enter a color for the text!',
-        name: 'textColor',
-    },
-    {
-        type: 'list',
-        message: 'Please pick a shape!',
-        choices: ['Circle', 'Triangle', 'Square'],
-        name: 'shape',
-    },
-    {
-        type: 'input',
-        message: 'Please enter a color for the shape!',
-        name: 'shapeColor',
-    },
-  ])
-  .then((response) =>
-    console.log(response)
-  );
-  
+const questions = [
+  {
+    type: "list",
+    message: "What shape would you like?",
+    choices: ["Circle", "Square", "Triangle"],
+    name: "shape"
+  },
+  {
+    type: "input",
+    message: "What color would you like for your shape?",
+    name: "shapecolor"
+  },
+  {
+    type: "input",
+    message: "What text would you like? (Please input 3 characters!)",
+    name: "text"
+  },
+  {
+    type: "input",
+    message: "What color would you like for your text?",
+    name: "textcolor"
+  },
+];
+
+inquirer.prompt(questions).then(res=>{
+  let shape;
+  if(res.shape==="Circle"){
+      shape=new Circle(res.shapecolor, res.text, res.textcolor)
+  };
+  if(res.shape==="Triangle"){
+      shape=new Triangle(res.shapecolor, res.text, res.textcolor)
+  }; 
+  if(res.shape==="Square"){
+      shape=new Square(res.shapecolor, res.text, res.textcolor)
+  };
+  fs.writeFileSync("./examples/logo.svg",shape.render())
+})
